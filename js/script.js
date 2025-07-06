c78a42f5b1c5d8d6dfb7d27b1902759a0c3abfa8
D3M0N63 <tobiast963@gmail.com>
2025-07-06 01:34:58 -0300

    edicion de codigos

--- js/script.js
+++ js/script.js
@@ -395,7 +395,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- Lógica para el Efecto de Lupa ---
     let lens = null; // La lupa
     const zoomFactor = 1; // Factor de ampliación
-    const offset = 250; // Desplazamiento de la lupa desde el cursor (en píxeles)
+    const offset = 200; // Desplazamiento de la lupa desde el cursor (en píxeles)
 
     // Esta función ahora solo es llamada por openZoomModal para la imagen ampliada
     function setupMagnifyingGlassListeners(imgToAttachTo) {
--- js/search-results-script.js
+++ js/search-results-script.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         productsToDisplay.forEach(product => {
             const productNameForWhatsapp = product.name || 'un neumático';
             const whatsappMessage = encodeURIComponent(`Hola! Me interesa el neumático ${productNameForWhatsapp} que vi en su web. ID: ${product.id}`);
-            const whatsappLink = `https://wa.me/595XXXXXXXXX?text=${whatsappMessage}`;
+            const whatsappLink = `https://wa.me/595983068998?text=${whatsappMessage}`;
 
             const productCardHtml = `
                 <div class="product-result-card w-full">
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // --- Lógica para el Efecto de Lupa ---
     let lens = null;
     const zoomFactor = 1; // Factor de ampliación
-    const offset = 250; // Desplazamiento de la lupa desde el cursor (en píxeles)
+    const offset = 200; // Desplazamiento de la lupa desde el cursor (en píxeles)
 
     // Esta función ahora solo es llamada por openZoomModal para la imagen ampliada
     function setupMagnifyingGlassListeners(imgToAttachTo) {
